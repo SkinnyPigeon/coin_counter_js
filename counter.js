78d16063d0f64c82a57add976bb4673abd46266e
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-07-15 17:22:05 +0100

    got a slightly long winded but working solution

--- counter.js
+++ counter.js
@@ -1,14 +1,22 @@
-var cash = 100
+var cash = 12.58
 
-var pounds = Math.floor( cash / 100 )
-var fifties = Math.floor( cash % 100 / 50 ) 
-var twenties = Math.floor( cash % 100 % 50 / 20 ) 
-var tens = Math.floor( cash % 100 % 50 % 20 / 10 ) 
-var fives = Math.floor( cash % 100 % 50 % 20 % 10 / 5 ) 
-var twos = Math.floor( cash % 100 % 50 % 20 % 10 % 5 / 2) 
-var ones = Math.floor( cash % 100 % 50 % 20 % 10 % 5 % 2 / 1 ) 
+var multiply = 100 * cash
 
-console.log( "Total(p): " + cash )
+var tenPounds = Math.floor( multiply / 1000 )
+var fivePounds = Math.floor( multiply % 1000 / 500 )
+var twoPounds = Math.floor( multiply % 1000 % 500 / 200 )
+var pounds = Math.floor( multiply % 1000 % 500 % 200 / 100 )
+var fifties = Math.floor( multiply % 1000 % 500 % 200 % 100 / 50 ) 
+var twenties = Math.floor( multiply % 1000 % 500 % 200 % 100 % 50 / 20 ) 
+var tens = Math.floor( multiply % 1000 % 500 % 200 % 100 % 50 % 20 / 10 ) 
+var fives = Math.floor( multiply % 1000 % 500 % 200 % 100 % 50 % 20 % 10 / 5 ) 
+var twos = Math.floor( multiply % 1000 % 500 % 200 % 100 % 50 % 20 % 10 % 5 / 2) 
+var ones = Math.floor( multiply % 1000 % 500 % 200 % 100 % 50 % 20 % 10 % 5 % 2 / 1 ) 
+
+console.log( "Total: £" + cash )
+console.log( "£10: " + tenPounds )
+console.log( "£5: " + fivePounds )
+console.log( "£2: " + twoPounds )
 console.log( "£1: " + pounds )
 console.log( "50p: " + fifties )
 console.log( "20p: " + twenties )
